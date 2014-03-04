2073eccbdddd9c0d4fdd4d6cabd1997d1c22162e
Zach Olauson <zmolauson@gmail.com>
2014-03-04 10:33:55 -0600

    cleanup hiccup templates and redirect to root if no gamestate in session

--- src/clj/ttt_clojure_web/controller/game_controller.clj
+++ src/clj/ttt_clojure_web/controller/game_controller.clj
@@ -45,24 +45,32 @@
 (defn gamestate [request]
   (:gamestate (session request)))
 
+(defn row-size [board]
+  (math/sqrt (count board)))
+
 (defn index-board [board]
   (map-indexed #(if (= :- %2) (format "%2s" %1) (format "%2s" (name %2))) board))
+
+(defn partitioned-board [gamestate]
+  (partition (row-size (:board gamestate)) (index-board (:board gamestate))))
 
 (defn let-computer-move [gamestate]
   (move gamestate (find-move gamestate)))
 
+(defn computer-move-if-computer-turn [gamestate]
+  (if (= :x (:computer gamestate))
+      (let-computer-move gamestate)
+      gamestate))
+
 (defn start-game [request params]
   (let [gamestate (build-gamestate params)
-        gamestate (if (= :x (:computer gamestate)) (let-computer-move gamestate) gamestate)]
+        gamestate (computer-move-if-computer-turn gamestate)]
        (assoc (redirect "/play") :session (set-gamestate-session request gamestate))))
-
-(defn row-size [board]
-  (math/sqrt (count board)))
 
 (defn build-map-to-send-to-play-template [request]
   {:gamestate      (gamestate request)
    :possible-moves (possible-moves (gamestate request))
-   :indexed-board  (partition (row-size (:board (gamestate request))) (index-board (:board (gamestate request))))
+   :indexed-board  (partitioned-board (gamestate request))
    :game-over?     (game-over? (gamestate request))
    :winner         (winner (gamestate request))})
 
@@ -74,5 +82,5 @@
 
 (defroutes game-controller
   (POST "/new-game" {params :params} (start-game *request* params))
-  (GET  "/play"     []               (render-template "play" (build-map-to-send-to-play-template *request*)))
+  (GET  "/play"     []               (if (not (gamestate *request*)) (redirect "/") (render-template "play" (build-map-to-send-to-play-template *request*))))
   (POST "/move"     {params :params} (make-human-move params *request*)))
