f364a4931b4e01351e1b71c0ea6e6905e7002700
Zach Olauson <zmolauson@gmail.com>
2014-03-07 15:38:38 -0600

    update views

--- src/clj/ttt_clojure_web/helper/view_helper.clj
+++ src/clj/ttt_clojure_web/helper/view_helper.clj
@@ -6,3 +6,12 @@
              [joodo.env :as env]
              [joodo.middleware.asset-fingerprint :refer [add-fingerprint]]
              [joodo.views :refer [render-partial *view-context*]]))
+
+(defn space-taken? [string]
+  (if (re-find #"x|o" string) true false))
+
+(defn buttonify [string]
+  (if (space-taken? string) string
+    [:div 
+     (label string string)
+     (radio-button :move true string)]))
