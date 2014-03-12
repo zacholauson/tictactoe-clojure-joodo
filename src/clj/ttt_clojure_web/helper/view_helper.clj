e9862391cd5c16fb71fe0d9adccfc28925a2c13d
Zach Olauson <zmolauson@gmail.com>
2014-03-12 15:53:13 -0500

    cleanup view helper

--- src/clj/ttt_clojure_web/helper/view_helper.clj
+++ src/clj/ttt_clojure_web/helper/view_helper.clj
@@ -1,10 +1,10 @@
 (ns ttt-clojure-web.helper.view-helper
-   (:require [hiccup.element :refer :all]
-             [hiccup.form :refer :all]
-             [hiccup.page :refer :all]
-             [joodo.env :as env]
+   (:require [hiccup.element                     :refer :all]
+             [hiccup.form                        :refer :all]
+             [hiccup.page                        :refer :all]
+             [joodo.env                          :as env]
              [joodo.middleware.asset-fingerprint :refer [add-fingerprint]]
-             [joodo.views :refer [render-partial *view-context*]]))
+             [joodo.views                        :refer [render-partial *view-context*]]))
 
 (defn space-taken? [string]
   (if (re-find #"x|o" string) true false))
@@ -12,7 +12,8 @@
 (defn buttonify [string]
   (if (space-taken? string) string
     (if (:game-over? *view-context*) string
-      [:div {:class "move-form"}
-       (form-to [:post "/move"]
-         (text-field {:type :hidden} :move (clojure.string/trim string))
-         (submit-button {:class "move"} (clojure.string/trim string)))])))
+      (let [string (clojure.string/trim string)]
+        [:div {:class "move-form"}
+          (form-to [:post "/move"]
+            (text-field    {:type :hidden} :move string)
+            (submit-button {:class "move"}       string))]))))
